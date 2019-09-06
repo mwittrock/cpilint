549cb1932784e53a531cae4ecd2fd53a4725a346
Morten Wittrock <mwittrock@gmail.com>
2019-09-06 23:17:59 +0200

    Added a Bash launch script
    A Bash shell launch script was added, and the version number was bumped to 1.0.1.

--- src/dk/mwittrock/cpilint/CliClient.java
+++ src/dk/mwittrock/cpilint/CliClient.java
@@ -37,7 +37,7 @@ public final class CliClient {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CliClient.class);
 	private static final String PRODUCT_NAME = "CPILint";
-	private static final String VERSION = "1.0.0";
+	private static final String VERSION = "1.0.1";
 	private static final String COPYRIGHT = "2019 Morten N. Wittrock";
 	private static final int EXIT_STATUS_SUCCESS = 0;
 	private static final int EXIT_STATUS_ISSUES = 1;

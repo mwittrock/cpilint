a47a161e5936136d866a455116f483c2af5fe77c
Morten Wittrock <mwittrock@gmail.com>
2021-03-13 18:10:27 +0100

    Version numbers updated to 1.0.2

--- src/dk/mwittrock/cpilint/CliClient.java
+++ src/dk/mwittrock/cpilint/CliClient.java
@@ -37,8 +37,8 @@ public final class CliClient {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CliClient.class);
 	private static final String PRODUCT_NAME = "CPILint";
-	private static final String VERSION = "1.0.1";
-	private static final String COPYRIGHT = "2019 Morten N. Wittrock";
+	private static final String VERSION = "1.0.2";
+	private static final String COPYRIGHT = "2019-2021 Morten N. Wittrock";
 	private static final int EXIT_STATUS_SUCCESS = 0;
 	private static final int EXIT_STATUS_ISSUES = 1;
 	private static final int EXIT_STATUS_ERRORS = 2;
